34c56d0365766ffc9fc6b7149b9f0ae02f506efa
HyunJoo <38845396+hjpark1397@users.noreply.github.com>
2020-11-05 22:25:30 +0900

    first commit
    Which I studied

---
+++ FileClient.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FileGUITCP
+{
+    partial class FileClient
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFileSelect = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.txtFileSrc = new System.Windows.Forms.TextBox();
+            this.btnFileSend = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFileSelect
+            // 
+            this.btnFileSelect.Location = new System.Drawing.Point(357, 28);
+            this.btnFileSelect.Name = "btnFileSelect";
+            this.btnFileSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSelect.TabIndex = 0;
+            this.btnFileSelect.Text = "파일 선택";
+            this.btnFileSelect.UseVisualStyleBackColor = true;
+            this.btnFileSelect.Click += new System.EventHandler(this.btnFileSelect_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(24, 25);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(33, 12);
+            this.lblFile.TabIndex = 1;
+            this.lblFile.Text = "파일 ";
+            // 
+            // txtFileSrc
+            // 
+            this.txtFileSrc.Location = new System.Drawing.Point(63, 25);
+            this.txtFileSrc.Name = "txtFileSrc";
+            this.txtFileSrc.Size = new System.Drawing.Size(288, 21);
+            this.txtFileSrc.TabIndex = 4;
+            // 
+            // btnFileSend
+            // 
+            this.btnFileSend.Location = new System.Drawing.Point(12, 75);
+            this.btnFileSend.Name = "btnFileSend";
+            this.btnFileSend.Size = new System.Drawing.Size(75, 23);
+            this.btnFileSend.TabIndex = 14;
+            this.btnFileSend.Text = "파일 전송";
+            this.btnFileSend.UseVisualStyleBackColor = true;
+            this.btnFileSend.Click += new System.EventHandler(this.btnFileSend_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(112, 77);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(167, 21);
+            this.txtAddress.TabIndex = 15;
+            this.txtAddress.Text = "127.0.0.1";
+            // 
+            // FileClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 144);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.btnFileSend);
+            this.Controls.Add(this.txtFileSrc);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.btnFileSelect);
+            this.Name = "FileClient";
+            this.Text = "FileClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFileSelect;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.TextBox txtFileSrc;
+        private System.Windows.Forms.Button btnFileSend;
+        private System.Windows.Forms.TextBox txtAddress;
+    }
+}
+
---
+++ FileClient.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace FileGUITCP
+{
+    public partial class FileClient : Form
+    {
+        Socket mainSock, fileSock;
+        IPAddress thisAddress = IPAddress.Parse("127.0.0.1");
+        int port = 15000;
+        int fPort= 15001;
+        string FileSrc = "";
+        FileInfo FileInfo = null;
+        static ManualResetEvent manualEvent = new ManualResetEvent(false);
+        public FileClient()
+        {
+            InitializeComponent();
+            mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            fileSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+        }
+
+        private void btnFileSelect_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileSrc = ofd.FileName;
+                    txtFileSrc.Text = FileSrc;
+                    FileInfo = new FileInfo(FileSrc);
+                }
+            }
+
+        }
+
+
+        private void btnFileSend_Click(object sender, EventArgs e)
+        {
+            if (mainSock.IsBound) return; //이미 연결
+   
+            if (FileInfo == null) return;
+
+            // 서버에 연결
+            try
+            {
+                mainSock.Connect(thisAddress, port);
+                Console.WriteLine( "서버 연결:");
+                string fileLength = FileInfo.Length.ToString();
+                string fileName = FileInfo.Name;
+
+                byte[] bDts = Encoding.UTF8.GetBytes(
+                   fileName + ":" + fileLength + ":");
+
+                // 서버에 전송한다.
+                mainSock.Send(bDts);
+                Console.WriteLine("파일 정보 송신");
+                byte[] bDtsRx = new byte[4096];
+                mainSock.BeginReceive(bDtsRx, 0, 4096, SocketFlags.None, DataReceived, bDtsRx);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("연결에 실패했습니다!\n오류 내용: {0}",  ex.Message);
+                return;
+            }
+            Thread th = new Thread(FileTXClient);
+            th.Start();
+
+
+        }
+
+        void FileTXClient()
+        {
+            try
+            {
+                fileSock.Connect(thisAddress, fPort);
+                Console.WriteLine("file서버 연결:");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("file server 연결에 실패했습니다!\n오류 내용: {0}", ex.Message);
+                return;
+            }
+
+            manualEvent.WaitOne();
+
+            //FileInfo = new FileInfo(@"C:\Welcome to Show.show");
+            long fLen = FileInfo.Length;
+            string fileName = FileInfo.FullName;
+            byte[] bDts = new byte[4096];
+            Console.WriteLine(fileName);
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                if (!mainSock.IsBound) return;
+                int received = 0;
+                Console.WriteLine("파일을 송신 시작");
+                while (received < fLen)
+                {
+                    received += fs.Read(bDts, 0, bDts.Length);
+                    fileSock.Send(bDts, 0, bDts.Length, 0);
+                    Array.Clear(bDts, 0, bDts.Length);
+                }
+                fs.Close();
+                Console.WriteLine("파일을 수신 완료");
+            }
+        }
+        void DataReceived(IAsyncResult ar)
+        {
+            // BeginReceive에서 추가적으로 넘어온 데이터를 AsyncObject 형식으로 변환한다.
+            byte[] bDts = (byte []) ar.AsyncState;
+
+            // 데이터 수신을 끝낸다.
+            int received = mainSock.EndReceive(ar);
+
+            // 받은 데이터가 없으면(연결끊어짐) 끝낸다.
+            if (received <= 0)
+            {
+                mainSock.Disconnect(false);
+                mainSock.Close();
+                return;
+            }
+            string text = Encoding.UTF8.GetString(bDts, 0, received).Trim();
+            string [] token = text.Split(':');
+            Console.WriteLine(text + ":");
+           if (token[0].Equals("OK"))
+                     manualEvent.Set();
+
+        }
+    }
+}
